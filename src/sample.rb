f1df4aa1a34b43d1257cb622b019b9aba41af76e
Yvan Nyonzima <yvan.niyo@gmail.com>
2014-04-09 20:42:32 +0100

    first commit

---
+++ src/sample.rb
@@ -0,0 +1,14 @@
+puts 'Hello People'
+for i in 0..3
+  puts 'the index is %s' %i
+end
+
+a = [1,'hello',3.14]
+
+puts "\nThe first element is #{a[0]} \n"
+a[4]= 'Yvan'
+puts "The array is now #{a.inspect}\n"
+
+
+
+
