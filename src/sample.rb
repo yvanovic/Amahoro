16abe8a5ca68a93088b32702f4b4b13ebc10cedc
Diana Vadai <diana.vadai@gmail.com>
2015-10-16 15:09:43 +0100

    Changed name

--- src/sample.rb
+++ src/sample.rb
@@ -26,6 +26,6 @@ end
 person_1 = Person.new("Dia",54,1.60)
 puts "\nThe body mass index of %s"%person_1.name + ' is %.2f'%person_1.bmi()
 
-person_2 = Person.new("Jesus",85,1.65)
+person_2 = Person.new("Dia2",85,1.65)
 puts "\nThe body mass index of %s"%person_2.name + ' is %.2f'%person_2.bmi()
 
