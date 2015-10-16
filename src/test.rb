939e626a636bb5e29ced9149c31465e6a277057f
Nate Ballantyne <nathan.ballantyne@hailocab.com>
2015-10-16 15:17:05 +0100

    Made a script for some information

---
+++ src/test.rb
@@ -0,0 +1,12 @@
+print "What is your name?:"
+first_name = gets.chomp.downcase.capitalize
+
+print "What is your surname?:"
+surname = gets.chomp.downcase.capitalize
+
+print "What is your favourite colour"
+favourite_colour = gets.chomp.downcase.capitalize
+
+puts "Your name is: #{first_name} #{surname}, and your favourite colour is #{favourite_colour}. Your name is #{first_name.length} letters long."
+
+
